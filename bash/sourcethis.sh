899372131edbc6bae86898d0a979f33654afcda0
ipa-mig <matthias.gruhler@ipa.fraunhofer.de>
2014-01-22 13:00:01 +0100

    some convenience improvments to sourcthis.sh

--- bash/sourcethis.sh
+++ bash/sourcethis.sh
@@ -10,23 +10,28 @@ DIR=$CWD
 NOTFOUND=true
 
 echo " "
+
+# search recursively through folders until we run into the homedirectory
 while test $CWD != $HOME && $NOTFOUND ; do
 	if [ -f "$CWD/devel/setup.bash" ] ;
 	then
 		source $CWD/devel/setup.bash
 		echo "sourced $CWD/devel/setup.bash"
 		NOTFOUND=false
+	else	
+		pushd .. > /dev/null
+		CWD=$(pwd)
 	fi
-	pushd .. > /dev/null
-	CWD=$(pwd)
-	
 done
-echo " "
 
+# if we didn't find a catkin workspace source bashrc, just in case
 if $NOTFOUND;
 then
 	echo "Not in catkin workspace, sourcing .bashrc"
 	source ~/.bashrc
 fi
 
+echo " "
+
+# get back old workind directory
 pushd $DIR > /dev/null
