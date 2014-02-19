6dbadf72ef982a76ccfc67e01a97051ccc160117
ipa-mig <Matthias.Gruhler@ipa.fraunhofer.de>
2014-02-19 12:40:42 +0100

    change default robot

--- ros/ros_common.sh
+++ ros/ros_common.sh
@@ -1,6 +1,6 @@
 #set ROS environment variables
 export ROS_EMAIL=mig@ipa.fhg.de
-export ROBOT=cob3-3
+export ROBOT=raw3-1
 #export ROBOT_ENV=ipa-kitchen
 #export ROBOT_ENV=ipa-4th-floor
 export ROBOT_ENV=ipa-apartment
