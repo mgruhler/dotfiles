2b128636785aeca03dd9e33d4e80337401c8e1b7
ipa-mig <matthias.gruhler@ipa.fraunhofer.de>
2014-02-21 16:10:34 +0100

    cleanup ros shell scripts

--- ros/ros_fuerte.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-source /opt/ros/fuerte/setup.bash
-export ROS_PACKAGE_PATH=~/git/fuerte_backup:$ROS_PACKAGE_PATH
-export ROS_WORKSPACE=~/git/fuerte_backup
--- ros/ros_groovy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-source /opt/ros/groovy/setup.bash
-export ROS_PACKAGE_PATH=~/git/groovy_rosbuild:$ROS_PACKAGE_PATH
-export ROS_WORKSPACE=~/git/groovy_rosbuild
